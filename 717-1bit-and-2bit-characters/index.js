f10f99ea6670e8561448b15588bd9df17a2b177a
FuNn1esT <admin@funn1est.cn>
2018-09-27 22:50:00 +0800

    update

---
+++ 717-1bit-and-2bit-characters/index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+const isOneBitCharacter = bits => {
+  const len = bits.length;
+  if (len === 1) {
+    return bits[0] === 0;
+  }
+  const loopLen = len - 1;
+  let i = 0;
+  while (i < loopLen) {
+    if (bits[i] === 1) {
+      i += 2;
+    } else {
+      i += 1;
+    }
+  }
+  return i === loopLen;
+};
+
+const isOneBitCharacter1 = bits => /^(0|10|11)*0$/.test(bits.join(''));
+
+console.log(isOneBitCharacter([1]));
+console.log(isOneBitCharacter([1, 0, 0]));
+console.log(isOneBitCharacter([1, 1, 0]));
+console.log(isOneBitCharacter([1, 1, 1, 0]));
+console.log('------');
+console.log(isOneBitCharacter1([1]));
+console.log(isOneBitCharacter1([1, 0, 0]));
+console.log(isOneBitCharacter1([1, 1, 0]));
+console.log(isOneBitCharacter1([1, 1, 1, 0]));
