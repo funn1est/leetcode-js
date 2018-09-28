73b2d16cba0872dfc3400697be18c7a81baadd43
FuNn1esT <admin@funn1est.cn>
2018-09-28 23:15:09 +0800

    update

---
+++ 697-degree-of-an-array/index.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findShortestSubArray = nums => {
+  const map = {};
+  let degree = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const value = nums[i];
+    if (!map[value]) {
+      map[value] = { start: i, end: i, count: 1 };
+    } else {
+      map[value].end = i;
+      map[value].count += 1;
+    }
+    if (map[value].count > degree) {
+      degree = map[value].count;
+    }
+  }
+
+  let result = Number.MAX_SAFE_INTEGER;
+  const mapValues = Object.values(map);
+  for (let i = 0; i < mapValues.length; i++) {
+    const { start, end, count } = mapValues[i];
+    if (count === degree) {
+      const size = end - start + 1;
+      if (size < result) {
+        result = size;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(findShortestSubArray([1, 2, 2, 3, 1]));
+console.log(findShortestSubArray([1, 2, 2, 3, 1, 4, 2]));
