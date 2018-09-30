c177956eb2a88181a06c7d338be67700f8293590
FuNn1esT <admin@funn1est.cn>
2018-09-30 22:51:35 +0800

    update

---
+++ 268-missing-number/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const missingNumber = nums => {
+  const len = nums.length;
+  let sum = 0;
+  let expected = len;
+  for (let i = 0; i < len; i++) {
+    sum += nums[i];
+    expected += i;
+  }
+  return expected - sum;
+};
+
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
