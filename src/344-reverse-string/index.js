8fa6a6fb8774d8fac571867eeab5d2cebc170f50
FuNn1esT <admin@funn1est.cn>
2019-02-14 21:44:08 +0800

    update

--- src/344-reverse-string/index.js
+++ src/344-reverse-string/index.js
@@ -5,3 +5,21 @@
 const reverseString = s => s.split('').reduce((str, s) => s + str, '');
 
 console.log(reverseString('hello'));
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString1 = s => {
+  const len = s.length;
+  const loopLen = Math.floor(len / 2);
+  for (let i = 0; i < loopLen; i += 1) {
+    const tail = len - i - 1;
+    const tmp = s[i];
+    s[i] = s[tail];
+    s[tail] = tmp;
+  }
+  console.log(s);
+};
+
+console.log(reverseString1(['h', 'e', 'l', 'l', 'o']));
