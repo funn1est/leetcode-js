54f1d7781286d7bc0bb3cefa90849fca94d9c315
FuNn1esT <admin@funn1est.cn>
2019-02-16 23:18:32 +0800

    update

---
+++ src/125-valid-palindrome/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = s => {
+  const str = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+  const len = str.length;
+  for (let i = 0; i < len / 2; i += 1) {
+    if (str[i] !== str[len - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+console.log(isPalindrome('race a car'));
+console.log(isPalindrome('abcba'));
+console.log(isPalindrome('abccba'));
