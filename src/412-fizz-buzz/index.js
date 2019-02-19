75141f8ca334b0e20d6bf07d9032db65b472516f
FuNn1esT <admin@funn1est.cn>
2019-02-19 22:43:20 +0800

    update

---
+++ src/412-fizz-buzz/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = n =>
+  [...Array(n)].map((_, index) => {
+    const val = index + 1;
+    if (val % 3 === 0 && val % 5 === 0) {
+      return 'FizzBuzz';
+    }
+    if (val % 3 === 0) {
+      return 'Fizz';
+    }
+    if (val % 5 === 0) {
+      return 'Buzz';
+    }
+    return val.toString();
+  });
+
+console.log(fizzBuzz(15));
