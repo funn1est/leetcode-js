cc3d03f5bf6a27dff3dca6e2164878f7f04b73eb
FuNn1esT <admin@funn1est.cn>
2019-02-20 23:30:02 +0800

    update

---
+++ src/326-power-of-three/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfThree = n => {
+  let x = n;
+  while (x > 1) {
+    x /= 3;
+  }
+  return x === 1;
+};
+
+console.log(isPowerOfThree(27));
+console.log(isPowerOfThree(0));
+console.log(isPowerOfThree(1));
+console.log(isPowerOfThree(9));
+console.log(isPowerOfThree(45));
