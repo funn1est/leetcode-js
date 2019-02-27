bc816fc5f63e8c0a248bce69c0d625fddc6be19d
FuNn1esT <admin@funn1est.cn>
2019-02-27 23:32:57 +0800

    update

---
+++ src/024-swap-nodes-in-pairs/index.js
@@ -0,0 +1,34 @@
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const swapPairs = head => {
+  if (head === null || head.next === null) {
+    return head;
+  }
+
+  const node = head.next;
+  head.next = swapPairs(head.next.next);
+  node.next = head;
+
+  return node;
+};
+
+const node1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+const node2 = new ListNode(1);
+
+console.log(swapPairs(node1));
+console.log(swapPairs(node2));
