027074330feb534e11bb1a91a2038787202bb5a2
FuNn1esT <admin@funn1est.cn>
2019-03-12 23:10:19 +0800

    update

---
+++ src/237-delete-node-in-a-linked-list/index.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+const deleteNode = node => {
+  node.val = node.next.val;
+  node.next = node.next.next;
+};
+
+const node1 = { val: 4, next: { val: 5, next: { val: 1, next: { val: 9, next: null } } } };
+const node2 = { val: 4, next: { val: 5, next: { val: 1, next: { val: 9, next: null } } } };
+
+deleteNode(node1.next);
+console.log(node1);
+
+deleteNode(node2.next.next);
+console.log(node2);
---
+++ src/ListNode.js
@@ -0,0 +1,6 @@
+export default class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
