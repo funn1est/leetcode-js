3b57c8cde2fa4427b0c3a816e7d4374adf6c7bed
FuNn1esT <admin@n1x.cn>
2019-03-14 23:50:24 +0800

    update

---
+++ src/020-valid-parentheses/index.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = s => {
+  const leftMap = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+
+  const stack = [];
+
+  for (const c of s) {
+    if (leftMap[c]) {
+      stack.push(leftMap[c]);
+    } else {
+      if (c !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+console.log(isValid('()'));
+console.log(isValid('(]'));
