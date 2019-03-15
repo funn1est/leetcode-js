57986cad5ab1d58f69ac537e9f12a3b39a3e764f
FuNn1esT <admin@n1x.cn>
2019-03-15 23:59:35 +0800

    update

---
+++ src/070-climbing-stairs/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = n => {
+  const memo = [];
+  const helper = (i, end) => {
+    if (i > end) {
+      return 0;
+    }
+    if (i === end) {
+      return 1;
+    }
+    if (memo[i]) {
+      return memo[i];
+    }
+    memo[i] = helper(i + 1, n) + helper(i + 2, n);
+    return memo[i];
+  };
+  return helper(0, n);
+};
+
+console.log(climbStairs(2));
+console.log(climbStairs(3));
+console.log(climbStairs(4));
