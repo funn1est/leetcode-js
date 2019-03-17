bfece5802403aa2506f6fe205446f5f29a066fd4
FuNn1esT <admin@n1x.cn>
2019-03-17 23:53:04 +0800

    update

---
+++ src/007-reverse-integer/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = x => {
+  if (x === 0) {
+    return 0;
+  }
+
+  const reverseX = parseInt(
+    Math.abs(x)
+      .toString(10)
+      .split('')
+      .reverse()
+      .join(''),
+    10,
+  );
+
+  const res = x > 0 ? reverseX : -reverseX;
+  if (res > 2147483648 || res < -2147483649) {
+    return 0;
+  }
+  return res;
+};
+
+console.log(reverse(123));
+console.log(reverse(-123));
+console.log(reverse(120));
+console.log(reverse(90100));
