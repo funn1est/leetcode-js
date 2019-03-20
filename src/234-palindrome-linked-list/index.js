e67df71812c89058ea31cd6fb93fc74aac2cb9fa
FuNn1esT <admin@n1x.cn>
2019-03-20 23:51:52 +0800

    update

---
+++ src/234-palindrome-linked-list/index.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const isPalindrome = head => {
+  let left = '';
+  let right = '';
+  while (head) {
+    left += head.val;
+    right = head.val + right;
+    head = head.next;
+  }
+  return left === right;
+};
+
+console.log(isPalindrome({ val: 1, next: { val: 2, next: null } }));
+console.log(
+  isPalindrome({ val: 1, next: { val: 2, next: { val: 2, next: { val: 1, next: null } } } }),
+);
