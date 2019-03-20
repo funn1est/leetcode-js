49ec9cad808d19251a203354b8052423aad9d276
FuNn1esT <admin@n1x.cn>
2019-03-20 23:41:34 +0800

    update

---
+++ src/191-number-of-1-bits/index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+const hammingWeight = n => Array.from(n.toString(2)).filter(i => i === '1').length;
+
+console.log(hammingWeight(00000000000000000000000000001011));
+console.log(hammingWeight(00000000000000000000000010000000));
