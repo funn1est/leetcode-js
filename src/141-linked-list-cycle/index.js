52a4bb9f285f8c21a831a70c6d80a2c0a94ad1b7
FuNn1esT <admin@n1x.cn>
2019-03-26 23:57:45 +0800

    update

---
+++ src/141-linked-list-cycle/index.js
@@ -0,0 +1,36 @@
+const ListNode = require('../ListNode');
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = head => {
+  const visited = [];
+  while (head) {
+    if (visited.includes(head)) {
+      return true;
+    }
+    visited.push(head);
+    head = head.next;
+  }
+  return false;
+};
+
+const node1 = { val: 3, next: null };
+const node11 = { val: 2, next: null };
+const node111 = { val: 0, next: null };
+const node1111 = { val: -4, next: null };
+node1.next = node11;
+node11.next = node111;
+node111.next = node1111;
+node1111.next = node11;
+
+console.log(hasCycle(node1));
