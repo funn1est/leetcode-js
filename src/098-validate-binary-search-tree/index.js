ce7dae1012a041248c439e36f6b0a42b218801fa
FuNn1esT <admin@n1x.cn>
2019-03-30 00:22:22 +0800

    update

---
+++ src/098-validate-binary-search-tree/index.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = root => {
+  if (!root) {
+    return true;
+  }
+
+  const helper = (node, min, max) => {
+    if (node.val <= min) {
+      return false;
+    }
+    if (node.val >= max) {
+      return false;
+    }
+    const left = node.left ? helper(node.left, min, node.val) : true;
+    if (left) {
+      const right = node.right ? helper(node.right, node.val, max) : true;
+      return right;
+    }
+    return false;
+  };
+  return helper(root, -Infinity, Infinity);
+};
+
+console.log(isValidBST({ val: 2, left: 1, right: 3 }));
+console.log(isValidBST({ val: 5, left: 1, right: { val: 4, left: 3, right: 6 } }));
