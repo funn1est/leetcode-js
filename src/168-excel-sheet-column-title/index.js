3ccbb1a72d5e04696877e0091a14678b5f94c895
FuNn1esT <admin@n1x.cn>
2019-04-18 23:14:52 +0800

    update

---
+++ src/168-excel-sheet-column-title/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+const convertToTitle = n => {
+  let str = '';
+
+  while (n > 0) {
+    str = `${String.fromCharCode(((n - 1) % 26) + 65)}${str}`;
+    n = Math.floor((n - 1) / 26);
+  }
+
+  return str;
+};
+
+console.log(convertToTitle(1));
+console.log(convertToTitle(28));
+console.log(convertToTitle(51));
+console.log(convertToTitle(52));
+console.log(convertToTitle(701));
